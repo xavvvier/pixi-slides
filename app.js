c94f627b98a184281232837d2fb05eeffcbc5a6b
xavvvier <xavvvier@users.noreply.github.com>
2019-11-25 17:31:54 -0400

    Slide navigation with 'p' and 'n' keys

--- app.js
+++ app.js
@@ -13,119 +13,171 @@ let Application = PIXI.Application,
    Texture = PIXI.Texture,
    TextureCache = PIXI.utils.TextureCache;
 
-let target = document.getElementById('game');
-let slideContainer;
+class App {
 
-let size = configuration.size;
-//Create a Pixi Application
-let app = new Application({width: size.width, height: size.height});
+   //HTML element to inject the PIXI Application
+   targetElement;
 
-let player;
-//Set the game state 
-let state = play;
+   //Holds the reference to the current slide container
+   slideContainer;
 
-let imageNames = {
-   player: 'images/player.json',
-   terrain: 'images/terrain.json'
-}
+   //Holds the current slide configuration
+   slide;
+   //Which slide index is selected
+   slideIndex;
 
-//Add the canvas to the document
-target.appendChild(app.view);
+   //The PIXI application
+   app;
 
-//load images and call the setup function when done
-loader.add([
-   imageNames.player,
-   imageNames.terrain,
-   'images/background/blue.png',
-   'images/background/brown.png',
-   'images/background/gray.png',
-   'images/background/green.png',
-   'images/background/pink.png',
-   'images/background/purple.png',
-   'images/background/yellow.png'
-]).load(setup);
+   //size of canvas element
+   size;
 
-function setup(){
-   slideContainer = drawSlide(0);
-   drawTerrain(slideContainer);
-   addPlayer(slideContainer);
+   player;
 
-   app.stage.addChild(slideContainer);
-   app.ticker.add(delta => gameLoop(delta));
-   let left = keyboard("ArrowLeft"),
-      up = keyboard("ArrowUp"),
-      right = keyboard("ArrowRight"),
-      down = keyboard("ArrowDown");
-   right.press = () => { player.moveRight(); }
-   right.release = () => { player.stopMoving(); }
-   left.press = () => { player.moveLeft(); }
-   left.release = () => { player.stopMoving(); }
-}
+   size = configuration.size;
 
-function drawTerrain(container) {
-   let slide = configuration.slides[0];
-   let terrain = slide.terrain;
-   let spriteMap = terrain.sprites;
-   let spriteSize = 16; /*expecting a square sprite*/
-   let columns = Math.floor(configuration.size.width/spriteSize); /*how many columns there are*/
-   for (var i = 0, len = terrain.map.length; i < len; i++) {
-      var x = (i % columns) * spriteSize;
-      var y = Math.floor(i / columns) * spriteSize;
-      var spriteCode = terrain.map[i];
-      if(spriteCode !== '') {
-         let spriteName = spriteMap[spriteCode];
-         var sprite = new Sprite(resources[imageNames.terrain].textures[spriteName]);
-         sprite.x = x;
-         sprite.y = y;
-         container.addChild(sprite);
+   constructor(targetElement){
+      this.targetElement = targetElement;
+      this.init();
+   }
+
+   init(){
+      this.size = configuration.size;
+      //Create the PIXI Application
+      this.app = new Application({width: this.size.width, height: this.size.height});
+      //atlas resources to load
+      this.imageNames = {
+         player: 'images/player.json',
+         terrain: 'images/terrain.json'
+      }
+
+      //Add the canvas to the document
+      this.targetElement.appendChild(this.app.view);
+
+      //load images and call the setup function when done
+      loader.add([
+         this.imageNames.player,
+         this.imageNames.terrain,
+         'images/background/blue.png',
+         'images/background/brown.png',
+         'images/background/gray.png',
+         'images/background/green.png',
+         'images/background/pink.png',
+         'images/background/purple.png',
+         'images/background/yellow.png'
+      ]).load(() => this.setup());
+   }
+
+   setup(){
+      //Draw the first slide
+      this.slideContainer = this.drawSlide(0);
+      this.app.ticker.add(delta => this.gameLoop(delta));
+      let left = keyboard("ArrowLeft"),
+         up = keyboard("ArrowUp"),
+         right = keyboard("ArrowRight"),
+         down = keyboard("ArrowDown");
+      right.press = () => { this.player.moveRight(); }
+      right.release = () => { this.player.stopMoving(); }
+      left.press = () => { this.player.moveLeft(); }
+      left.release = () => { this.player.stopMoving(); }
+      keyboard("n").press = () => { this.nextSlide();}
+      keyboard("p").press = () => { this.previousSlide();}
+   }
+
+   previousSlide(){
+      //Move to the previous slide if in 1 or above
+      if(this.slideIndex > 0) {
+         this.slideContainer.visible = false;
+         this.slideContainer.destroy();
+         this.slideContainer = this.drawSlide(this.slideIndex-1);
       }
    }
-}
 
-function drawSlide(number) {
-   let slide = configuration.slides[number];
-   let container = new Container();
-   //draw the background
-   let backgroundTexture = resources[slide.background].texture;
-   let tileHeight = backgroundTexture.baseTexture.height,
-      tileWidth = backgroundTexture.baseTexture.width;
-   let rows = Math.floor(size.height / tileHeight);
-   let cols = Math.floor(size.width / tileWidth);
-   for (var row = 0; row <= rows; row++) {
-      for (var col = 0; col <= cols; col++) {
-         let tile = new Sprite(backgroundTexture);
-         tile.y = row * tileHeight;
-         tile.x = col * tileWidth;
-         container.addChild(tile);
+   nextSlide() {
+      //Move to the next slide if there are more
+      let total = configuration.slides.length;
+      if(this.slideIndex < total-1){
+         this.slideContainer.visible = false;
+         this.slideContainer.destroy();
+         this.slideContainer = this.drawSlide(this.slideIndex+1);
       }
    }
-   //draw all the lines
-   let lines = slide.lines;
-   for (const line of lines) {
-      let text = new Text(line.content, {
-         fontFamily: 'pixellari', 
-         fontSize: line.size || 16, 
-         fill: line.color || 'black',
-      });
-      text.x = line.x || 0;
-      text.y = line.y || 0;
-      container.addChild(text);
+
+   drawSlide(number) {
+      this.slide = configuration.slides[number];
+      this.slideIndex = number;
+      let container = new Container();
+      this.drawBackground(container);
+      //draw all the lines
+      let lines = this.slide.lines;
+      for (const line of lines) {
+         let text = new Text(line.content, {
+            fontFamily: 'pixellari', 
+            fontSize: line.size || 16, 
+            fill: line.color || 'black',
+         });
+         text.x = line.x || 0;
+         text.y = line.y || 0;
+         container.addChild(text);
+      }
+      this.drawTerrain(container);
+      this.addPlayer(container);
+      //Add it to the app stage
+      this.app.stage.addChild(container);
+      return container;
    }
-   return container;
+
+   drawBackground(container) {
+      //draw the background
+      let backgroundTexture = resources[this.slide.background].texture;
+      let tileHeight = backgroundTexture.baseTexture.height,
+         tileWidth = backgroundTexture.baseTexture.width;
+      let rows = Math.floor(this.size.height / tileHeight);
+      let cols = Math.floor(this.size.width / tileWidth);
+      for (var row = 0; row <= rows; row++) {
+         for (var col = 0; col <= cols; col++) {
+            let tile = new Sprite(backgroundTexture);
+            tile.y = row * tileHeight;
+            tile.x = col * tileWidth;
+            container.addChild(tile);
+         }
+      }
+   }
+
+   drawTerrain(container) {
+      this.slide = configuration.slides[0];
+      let terrain = this.slide.terrain;
+      let spriteMap = terrain.sprites;
+      let spriteSize = 16; /*expecting a square sprite*/
+      let columns = Math.floor(configuration.size.width/spriteSize); /*how many columns there are*/
+      for (var i = 0, len = terrain.map.length; i < len; i++) {
+         var x = (i % columns) * spriteSize;
+         var y = Math.floor(i / columns) * spriteSize;
+         var spriteCode = terrain.map[i];
+         if(spriteCode !== '') {
+            let spriteName = spriteMap[spriteCode];
+            var sprite = new Sprite(resources[this.imageNames.terrain].textures[spriteName]);
+            sprite.x = x;
+            sprite.y = y;
+            container.addChild(sprite);
+         }
+      }
+   }
+
+
+   addPlayer(container){
+      //use https://www.leshylabs.com/apps/sstool/
+      let playerSp = resources[this.imageNames.player].spritesheet;
+      this.player = new Player(playerSp, container);
+   }
+
+   gameLoop(delta){
+      //Update the current game state
+      this.player.update();
+   }
 }
 
-function addPlayer(container){
-   //use https://www.leshylabs.com/apps/sstool/
-   let playerSp = resources[imageNames.player].spritesheet;
-   player = new Player(playerSp, container);
-}
 
-function gameLoop(delta){
-   //Update the current game state
-   state(delta);
-}
+new App(document.getElementById('game'));
 
-function play(delta){
-   player.update();
-}
 
--- configuration.js
+++ configuration.js
@@ -5,6 +5,13 @@ export const configuration = {
    },
    slides: [
       {
+         lines: [
+            {content: "Top left", size: 20, color: 'white',x:18, y:20},
+            {content: "Bottom Right", size: 32, x: 440, y: 420, color: 'red'},
+            {content: "Line 3", size: 32, x: 20, y: 140},
+            {content: "Line 4", size: 32, x: 20, y: 160},
+            {content: "Line 5", size: 32, x: 20, y: 180},
+         ],
          background: 'images/background/brown.png', /*select anyone in the images/background/ folder*/
          terrain: {
             map: 'aggggggggggggggggggggggggggggggggggggggb' +
@@ -56,14 +63,74 @@ export const configuration = {
                o: 'grassg-b',
                p: 'grassg-c'
             }
-         },
+         }
+      },
+      {
          lines: [
-            {content: "Top left", size: 20, color: 'white',x:18, y:20},
-            {content: "Bottom Right", size: 32, x: 440, y: 420, color: 'red'},
-            {content: "Line 3", size: 32, x: 20, y: 140},
-            {content: "Line 4", size: 32, x: 20, y: 160},
-            {content: "Line 5", size: 32, x: 20, y: 180},
-         ]
+            {content: "Slide 1", size: 20, color: 'white',x:18, y:20},
+            {content: "Item 1", size: 32, x: 440, y: 420, color: 'red'},
+            {content: "Line 2", size: 32, x: 20, y: 140}
+         ],
+         background: 'images/background/yellow.png', /*select anyone in the images/background/ folder*/
+         terrain: {
+            map: 'aggggggggggggggggggggggggggggggggggggggb' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'e                                      f' +
+                 'hnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnni' +
+                 'jooooooooooooooooooooooooooooooooooooook' 
+            ,
+            sprites: {
+               a: 'wood-a1',
+               b: 'wood-a2',
+               c: 'wood-a3',
+               d: 'wood-a4',
+               e: 'wood-r',
+               f: 'wood-l',
+               g: 'shingle-b',
+               h: 'grassg-tl',
+               i: 'grassg-tr',
+               j: 'grassg-bl',
+               k: 'grassg-br',
+               l: 'grassg-r',
+               m: 'grassg-l',
+               n: 'grassg-t',
+               o: 'grassg-b',
+               p: 'grassg-c'
+            }
+         }
+      },
+      {
+         lines: [
+            {content: "Slide 1", size: 20, color: 'white',x:18, y:20},
+            {content: "Item 1", size: 32, x: 440, y: 420, color: 'red'},
+            {content: "Line 2", size: 32, x: 20, y: 140}
+         ],
+         background: 'images/background/green.png', /*select anyone in the images/background/ folder*/
       }
    ]
 };
